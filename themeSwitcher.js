8ebc68f3201876a78665ad6e598e9131dce25411
palak-sonkar <70479356+palak-sonkar@users.noreply.github.com>
2021-04-04 13:24:05 +0530

    Update themeSwitcher.js

--- themeSwitcher.js
+++ themeSwitcher.js
@@ -1,8 +1,7 @@
 //Theme Switch
-const links = document.querySelectorAll(".alternate-style")
+const links=document.querySelectorAll(".alternate-style")
 totalLinks=links.length;
 function setActiveStyle(color){
-
   for(let i=0;i<totalLinks;i++){
     if(color===links[i].getAttribute("title")){
       links[i].removeAttribute("disabled")
